e230ebeb44c111f45b0daea0a4e731e1db01a2cf
oagarian <tns6@aluno.ifal.edu.br>
2025-06-23 14:29:22 -0300

    refactor: agents

--- python-backend/ec_agents/return_agent.py
+++ python-backend/ec_agents/return_agent.py
@@ -5,7 +5,8 @@ from context.ecommerce_context import ECommerceAgentContext
 from guardrails.relevance import relevance_guardrail
 from guardrails.jailbreak import jailbreak_guardrail
 from tools.initiate_return import initiate_return
-from tools.check_return_policy import check_return_policy
+from tools.validate_return import validate_return
+
 from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX  # type: ignore
 
 
@@ -22,11 +23,12 @@ def return_agent_instructions(
         "You are a Return Agent. Follow this routine:\n"
         f"1. The order number is {order_number}, the product_id is {product_id} and the reason is {reason}, \n"
         " If any of them are unknown, ask the customer to inform such \n"
-        f"2. The customer wants to return or exchange a product. Order number: {order_number}, Product ID: {product_id}.\n"
-        "3. Use the `check_return_policy` tool to verify if the product can be returned or exchanged.\n"
-        f"4. If allowed, ask the customer for the reason if missing.\n"
+        f"2. If the reason contains something like that make understand that: used the item or removed the seal, the product can't be returned. \n"
+        f"3. The customer wants to return or exchange a product. Order number: {order_number}, Product ID: {product_id}.\n"
+        "3. Use the `validate_return` tool to verify if the product can be returned or exchanged.\n"
+        f"4. If allowed, ask the customer for the reason if missing. And again, validate the policy of use\n"
         f"5. Next, use the `initiate_return` tool with the {order_number}, the {product_id} and {reason} to start the return process.\n"
-        "6. Provide the return label URL and status to the customer.\n"
+        "6. Provide the return ID to the customer.\n"
         "If the question is unrelated, transfer back to the order triage agent."
         "If the customer asks anything else, transfer back to the order triage agent."
     )
@@ -37,6 +39,6 @@ return_agent = Agent[ECommerceAgentContext](
     model="gpt-4.1",
     handoff_description="Processes return or exchange requests.",
     instructions=return_agent_instructions,
-    tools=[check_return_policy, initiate_return],
+    tools=[validate_return, initiate_return],
     input_guardrails=[relevance_guardrail, jailbreak_guardrail],
 )
--- python-backend/ec_agents/triage_agent.py
+++ python-backend/ec_agents/triage_agent.py
@@ -1,4 +1,4 @@
-from agents import Agent, handoff
+from agents import Agent, RunContextWrapper, handoff
 from ec_agents.order_status_agent import order_status_agent
 from ec_agents.return_agent import return_agent
 from ec_agents.product_faq_agent import product_faq_agent, on_product_faq_handoff
@@ -8,15 +8,29 @@ from guardrails.relevance import relevance_guardrail
 from guardrails.jailbreak import jailbreak_guardrail
 from context.ecommerce_context import ECommerceAgentContext
 from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX # type: ignore
+from tools.validate_user import validate_user
 
+def triage_agent_instructions(
+    run_context: RunContextWrapper[ECommerceAgentContext], agent: Agent[ECommerceAgentContext]
+) -> str:
+    ctx = run_context.context
+    user_email = ctx.customer_email or "[unknown]"
+    return (
+        f"{RECOMMENDED_PROMPT_PREFIX} "
+        "You are a helpful order triaging agent. You can use your tools to delegate questions to other appropriate agents.\n"
+        "You need to follow this routine: \n"
+        f"1. The user email is {user_email}. If is unknown, first of all, ask the customer for the email \n"
+        "2. Validate if the user_email is valid using the tool: `validate_user` passing the email\n"
+        "3. If the email is invalid, return to the first step, and ask the email again\n"
+        "4. If the email is valid, you can delegate the questions to other appropriate agents."
+    )
+    
 triage_agent = Agent[ECommerceAgentContext](
     name="Triage Agent",
     model="gpt-4.1",
     handoff_description="A order triage agent that can delegate a customer's request to the appropriate agent.",
-    instructions=(
-        f"{RECOMMENDED_PROMPT_PREFIX} "
-        "You are a helpful order triaging agent. You can use your tools to delegate questions to other appropriate agents."
-    ),
+    instructions=triage_agent_instructions,
+    tools=[validate_user],
     handoffs=[
         order_status_agent,
         return_agent,
